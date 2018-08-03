4d15d538a861354f500709c46bc998063bc7deab
Yoann Delpierre <yoann.delpierre@capitainetrain.com>
2018-08-03 19:48:15 +0200

    create customers components

---
+++ src/components/customers.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Customers.css';
+
+class Customers extends Component {
+    render() {
+    return (<div className="Customers"></div>);
+    }
+}
+
+export default Customers;
