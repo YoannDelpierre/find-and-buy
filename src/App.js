af21d7e70d2ca7567301c6121abb100ccde0e4a7
Yoann Delpierre <yoann.delpierre@capitainetrain.com>
2018-08-03 19:47:42 +0200

    use customers and properties datas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+import Customers from './components/customers';
+import Properties from './components/properties';
 import './App.css';
 
+const timeout = ms => new Promise(res => setTimeout(res, ms))
+
 class App extends Component {
+  constructor(args) {
+    super(...args);
+    this.state = {
+      isLoading: true,
+      customers: [],
+      properties: [],
+    };
+  }
+
+  async componentDidMount() {
+    const customers = await generateCustomers();
+    const properties = await generateProperties();
+
+    // Simulate loading
+    await timeout(2000);
+    this.setState({
+      isLoading: !this.state.isLoading,
+      customers,
+      properties,
+    });
+  }
+
   render() {
     return (
       <div className="App">
