4f6d9830a695eec77fa7726b4d858043015459fe
Yoann Delpierre <yoann.delpierre@capitainetrain.com>
2018-08-04 18:31:25 +0200

    update api

--- src/api/customers.js
+++ src/api/customers.js
@@ -1,19 +1,25 @@
 const Chance = require('chance');
 
-const generatUniqueCustomer = () => {
+const generatUniqueCustomer = (surface) => {
     const chance = new Chance();
     return {
         id: chance.guid(),
         lastName: chance.last(),
-        firstName: chance.name(),
-        phone: chance.phone({ country: 'fr' })
+        firstName: chance.first(),
+        phone: chance.phone({ country: 'fr' }),
+        email: chance.email(),
+        search: {
+            surface: chance.integer({ min: surface * .8, max: surface * 1.2 }),
+            budget: chance.integer({ min: surface * 5000, max: surface * 8500 }),
+            rooms: (Math.round((surface / 20)) * 100) / 100,
+        }
     }
 };
 
-export const generateCustomers = (count = 10) => {
+export const generateCustomers = (count = 10, surface) => {
     let customers = [];
     for(let i = 0; i < count; i++) {
-        customers.push(generatUniqueCustomer());
+        customers.push(generatUniqueCustomer(surface));
     }
     return customers;
 }
